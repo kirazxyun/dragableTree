85a857c498825b6f1ea91ac4f55fd8cf5f36b0f4
zhangxiaoyun <zhangxiaoyun0828@sina.cn>
2018-01-25 18:27:36 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <dragable-tree></dragable-tree>
+  </div>
+</template>
+
+<script>
+import DragableTree from './components/dragableTree'
+export default {
+  name: 'App',
+  components: {
+    DragableTree
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/dragableTree.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    假装自己是棵树
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dragable-tree'
+}
+</script>
+
+<style lang="less">
+</style>
